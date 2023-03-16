6e9a4da6b85e8a5ab8665598fd118dfc74c16fc1
Tommer <souirtommer@gmail.com>
2023-03-16 21:58:05 +0800

    Initial Commit

---
+++ AppleWatch Watch App/AppleWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleWatchApp.swift
+//  AppleWatch Watch App
+//
+//  Created by ST SE on 16/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
